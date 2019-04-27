cd8b54c36a0cabd4a013a27abfb1409188bbe73b
ljt <329311024@qq.com>
2019-04-27 19:58:24 +0800

    springBoot RabbitMQ消息生产者

---
+++ rabbitmq-springboot-producer/src/main/java/com/example/demo/config/ProducerConfig.java
@@ -0,0 +1,36 @@
+package com.example.demo.config;
+
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"com.example.demo.*"})
+public class ProducerConfig {
+
+    @Bean
+    public ConnectionFactory connectionFactory(){
+        CachingConnectionFactory factory = new CachingConnectionFactory();
+        factory.setAddresses("192.168.25.135:5672");
+        factory.setUsername("root");
+        factory.setPassword("root");
+        factory.setVirtualHost("/");
+        return factory;
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
+        rabbitAdmin.setAutoStartup(true);//设置为true,spring容器加载
+        return rabbitAdmin;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate2(ConnectionFactory connectionFactory) {
+        return new RabbitTemplate(connectionFactory);
+    }
+}
