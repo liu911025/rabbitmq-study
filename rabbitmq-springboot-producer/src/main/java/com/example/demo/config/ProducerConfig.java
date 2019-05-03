19e30d72cfbb333f3b505da959d4862339dc92bb
ljt <329311024@qq.com>
2019-05-03 13:54:01 +0800

    springBoot RabbitMQ消息生产者

--- rabbitmq-springboot-producer/src/main/java/com/example/demo/config/ProducerConfig.java
+++ rabbitmq-springboot-producer/src/main/java/com/example/demo/config/ProducerConfig.java
@@ -2,8 +2,6 @@ package com.example.demo.config;
 
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan({"com.example.demo.*"})
 public class ProducerConfig {
 
-    @Bean
+    /*@Bean
     public ConnectionFactory connectionFactory(){
         CachingConnectionFactory factory = new CachingConnectionFactory();
         factory.setAddresses("192.168.25.135:5672");
@@ -20,17 +18,6 @@ public class ProducerConfig {
         factory.setPassword("root");
         factory.setVirtualHost("/");
         return factory;
-    }
+    }*/
 
-    @Bean
-    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
-        RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
-        rabbitAdmin.setAutoStartup(true);//设置为true,spring容器加载
-        return rabbitAdmin;
-    }
-
-    @Bean
-    public RabbitTemplate rabbitTemplate2(ConnectionFactory connectionFactory) {
-        return new RabbitTemplate(connectionFactory);
-    }
 }
